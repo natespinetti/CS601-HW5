c7a57e9454a085691c46470b3ae0365dc61473ee
Nathan Spinetti <104846459+natespinetti@users.noreply.github.com>
2022-06-12 13:05:02 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
+let NA = data.data[0].degrees.gpa.data.type;
+
 async function getDegrees(url) {
     // fetch the url
     await fetch(url)
       //get your data here, and check for the response status. If it's not 200, throw an error
       .then((response) => response.json())
       .then((data) => {
-        if (data.data[0].degrees.gpa.data.type === 'N/A') {
+        if (NA == 'N/A') {
             document.write(
                 `My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.institution} in ${data.data[0].degrees.bachelors.data.date}.
                  I expect to graduate at the age of ${data.data[0].degrees.age.data.type}!`
@@ -19,4 +21,4 @@ async function getDegrees(url) {
       );
   }
   
-  getDegrees("./src/degrees.json");
+  getDegrees("src/degrees.json");
