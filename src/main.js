8a0ad1895c8e460a31e2223ae7761a99f5c7cea3
Nathan Spinetti <104846459+natespinetti@users.noreply.github.com>
2022-06-12 17:41:46 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ async function getDegrees(url) {
     //get your data here, and check for the response status. If it's not 200, throw an error
     .then((response) => response.json())
     .then((data) => {
-      getBachelors(data.data[0].degrees.bachelors.data);
-      getGradCert(data.data[0].degrees.gradcertificate.data);
-      getMasters(data.data[0].degrees.masters.data);
+      getDegreeData(data.data[0].degrees.bachelors.data);
+      getDegreeData(data.data[0].degrees.gradcertificate.data);
+      getDegreeData(data.data[0].degrees.masters.data);
     });
 }
 
-let getBachelors = (obj) => {
+let getDegreeData = (obj) => {
   const main = document.querySelector("main");
 
   const contain = document.createElement("div");
@@ -32,78 +32,14 @@ let getBachelors = (obj) => {
   a.href = `${obj.website}`;
 
   logo.src = `${obj.logo}`;
-  line1.textContent = `My first degree was a ${obj.type} from ${obj.institution} in ${obj.date}.`;
-  line2.textContent = `I had a GPA of ${obj.gpa}, graduating as ${obj.honors} at the age of ${obj.age}.`;
 
-  const section = document.createElement("section");
-  contain.appendChild(section);
-  section.appendChild(logo);
-  section.appendChild(line1);
-  section.appendChild(line2);
-  section.appendChild(a);
-
-  main.appendChild(contain);
-};
-
-let getGradCert = (obj) => {
-  const main = document.querySelector("main");
-
-  const contain = document.createElement("div");
-  contain.classList.add("contain");
-  contain.style.backgroundImage = `url(${obj.img})`;
-
-  const logo = document.createElement("img");
-  logo.classList.add("logo");
-  const bgimg = document.createElement("img");
-  bgimg.classList.add("bg");
-  const line1 = document.createElement("p");
-  line1.classList.add("line1");
-  const line2 = document.createElement("p");
-  line2.classList.add("line2");
-
-  const a = document.createElement("a");
-  const link = document.createTextNode("Check out the program!");
-  a.appendChild(link);
-  a.href = `${obj.website}`;
-
-  logo.src = `${obj.logo}`;
-  line1.textContent = `My next degree will be a ${obj.type} from ${obj.institution}, I expect to finish in ${obj.date}.`;
-  line2.textContent = `I expect to finish at the age of ${obj.age}.`;
-
-  const section = document.createElement("section");
-  contain.appendChild(section);
-  section.appendChild(logo);
-  section.appendChild(line1);
-  section.appendChild(line2);
-  section.appendChild(a);
-
-  main.appendChild(contain);
-};
-
-let getMasters = (obj) => {
-  const main = document.querySelector("main");
-
-  const contain = document.createElement("div");
-  contain.classList.add("contain");
-  contain.style.backgroundImage = `url(${obj.img})`;
-
-  const logo = document.createElement("img");
-  logo.classList.add("logo");
-  const bgimg = document.createElement("img");
-  bgimg.classList.add("bg");
-  const line1 = document.createElement("p");
-  line1.classList.add("line1");
-  const line2 = document.createElement("p");
-  line2.classList.add("line2");
-
-  const a = document.createElement("a");
-  const link = document.createTextNode("Check out the program!");
-  a.appendChild(link);
-  a.href = `${obj.website}`;
-
-  logo.src = `${obj.logo}`;
-  line1.textContent = `My final degree will be a ${obj.type} from ${obj.institution}, I expect to finish in ${obj.date}.`;
-  line2.textContent = `I expect to finish at the age of ${obj.age}.`;
+  if (obj.gpa === "N/A" && obj.honors === "N/A") {
+    line1.textContent = `My next degree will be a ${obj.type} from ${obj.institution}, I expect to finish in ${obj.date}.`;
+    line2.textContent = `I expect to finish at the age of ${obj.age}.`;
+  } else {
+    line1.textContent = `My first degree was a ${obj.type} from ${obj.institution} in ${obj.date}.`;
+    line2.textContent = `I had a GPA of ${obj.gpa}, graduating as ${obj.honors} at the age of ${obj.age}.`;
+  }
 
   const section = document.createElement("section");
   contain.appendChild(section);
